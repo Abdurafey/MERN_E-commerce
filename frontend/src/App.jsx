e4803d98223a8e007bb007b9e733efcc17f000ee
Abdur Rafay Ahmed <a_rafay01@hotmail.com>
2024-05-07 12:26:10 +0500

    project setup forntend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <div className="justify-center text-center mt-10 text-orange-700 font-bold">
+      rafay
+    </div>
+  )
+}
+
+export default App
