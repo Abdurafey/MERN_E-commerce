79e8698f209ace635ff786a5a1af9f4217f684f7
Abdur Rafay Ahmed <a_rafay01@hotmail.com>
2024-05-13 15:31:40 +0500

    added productLIst and Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import ProductList from './features/product/ProductList';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <ProductList/>
+      <Home>
+        
+      </Home>
     </div>
   );
 }
