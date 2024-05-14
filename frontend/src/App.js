56853ad307418511e01d1d744a59e6a2ab3cebf6
Abdur Rafay Ahmed <a_rafay01@hotmail.com>
2024-05-14 19:07:37 +0500

    add auth feature, login & signUP page, routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,39 @@
 import './App.css';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Home/>),
+  },
+  {
+    path: "/login",
+    element: <LoginPage/>,
+  },
+  {
+    path: "/signUp",
+    element: <SignUpPage/>,
+  },
+]);
+
 
 function App() {
   return (
     <div className="App">
-      <Home>
-        
-      </Home>
+      {/* <Home>
+      </Home> */}
+      {/* <LoginPage/> */}
+      {/* <SignUpPage/> */}
+      <RouterProvider router={router} />
     </div>
   );
 }
